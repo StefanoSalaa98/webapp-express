4aea03338d5df4a5c76a4647465b28d9f707650d
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-30 18:45:34 +0100

    Aggiungi env

--- data/db.js
+++ data/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // uso il metodo di creazione oggetto di connessione
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'movie_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 // avvio la connesione tramite il metodo connect
